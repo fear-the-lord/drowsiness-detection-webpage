362bf7a50bbb3327adfafe76518a91962a0548e2
Rajarsi Saha <rajarsi3997@gmail.com>
2020-09-27 14:29:18 +0530

    Commit

--- mail.php
+++ mail.php
@@ -1,27 +1,22 @@
 <?php
-    $to = 'demo@spondonit.com';
-    $firstname = $_POST["fname"];
-    $email= $_POST["email"];
+    $to = 'rajarsi3997@gmail.com';
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
     $text= $_POST["message"];
-    $phone= $_POST["phone"];
     
-
-
     $headers = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
+    $headers .= "From: " . $email . "\r\n";
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
     $message ='<table style="width:100%">
-        <tr>
-            <td>'.$firstname.'  '.$laststname.'</td>
-        </tr>
+        <tr><td>Name: '.$name.'</td></tr>
         <tr><td>Email: '.$email.'</td></tr>
-        <tr><td>phone: '.$phone.'</td></tr>
+        <tr><td>Subject: '.$subject.'</td></tr>
         <tr><td>Text: '.$text.'</td></tr>
-        
     </table>';
 
-    if (@mail($to, $email, $message, $headers))
+    if (@mail($to, 'Message from contact page', $message, $headers))
     {
         echo 'The message has been sent.';
     }else{
