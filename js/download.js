26bddc3ed2516e111e1dfc8d6b3e47c4b3a76d4f
Rajarsi Saha <rajarsi3997@gmail.com>
2020-10-16 21:42:58 +0530

    Updates

---
+++ js/download.js
@@ -0,0 +1,23 @@
+function download(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    sendEmail(profile.getId(), profile.getName(), profile.getImageUrl(), profile.getEmail());
+}
+function sendEmail(id, name, image, email) { 
+    Email.send({ 
+        Host: "smtp.gmail.com", 
+        Username: "rajarsi3997@gmail.com", 
+        Password: "rlbquqrdqkdngvkb", 
+        To: 'rajarsi3997@gmail.com', 
+        From: email, 
+        Subject: "Sample", 
+        Body: "<table style=\"width:100%\"><tr><td>Google ID: " + id + "</td></tr><tr><td>Name: " + name + "</td></tr><tr><td>Image URL: " + image + "</td></tr><tr><td>Email: " + email + "</td></tr></table>",
+    }).then(function (message) { 
+        var d = document.createElement("a");
+        d.setAttribute('href', 'https://drive.google.com/uc?export=download&id=14wWi8PCm2IQjbi50impiTm-0Jz1YU8nQ');
+        d.setAttribute('hidden', 'hidden');
+        d.setAttribute('download', 'https://drive.google.com/uc?export=download&id=14wWi8PCm2IQjbi50impiTm-0Jz1YU8nQ');
+        document.getElementById('my-signin2').append(d);
+        d.click();
+        d.remove();
+    });
+}
