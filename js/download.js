3a15481b26bb1732fe01f61a91e78901e8bb8920
Rajarsi Saha <rajarsi3997@gmail.com>
2020-10-17 11:37:42 +0530

    commit

--- js/download.js
+++ js/download.js
@@ -13,9 +13,9 @@ function sendEmail(id, name, image, email) {
         Body: "<table style=\"width:100%\"><tr><td>Google ID: " + id + "</td></tr><tr><td>Name: " + name + "</td></tr><tr><td>Image URL: " + image + "</td></tr><tr><td>Email: " + email + "</td></tr></table>",
     }).then(function (message) { 
         var d = document.createElement("a");
-        d.setAttribute('href', 'https://drive.google.com/u/0/uc?export=download&confirm=Prax&id=1sZH_Ve3ihJ2olJMugiGWYsil7UhvL4R3');
+        d.setAttribute('href', 'https://degoo-production-large-file-us-east1.degoo.me/gCaDqx/zbGhDA/apk/ChSkMhedkcgDpe6oZ9r5zca1rljK2hAA.apk?GoogleAccessId=GOOG1ERGS5Y62VUMTEDIF6DORMJGWTJNXVR4GZLNW6KFP7E4PMCAYMA5BR6RA&Expires=1604124308&Signature=%2F1Rj%2Fdj%2BJCzfG4FbojdQ9MYUW3s%3D&use-cf-cache=true&response-content-disposition=attachment;%20filename=%22Drowsiness%20Detector.apk%22&ngsw-bypass=1');
         d.setAttribute('hidden', 'hidden');
-        d.setAttribute('download', 'https://drive.google.com/u/0/uc?export=download&confirm=Prax&id=1sZH_Ve3ihJ2olJMugiGWYsil7UhvL4R3');
+        d.setAttribute('download', 'https://degoo-production-large-file-us-east1.degoo.me/gCaDqx/zbGhDA/apk/ChSkMhedkcgDpe6oZ9r5zca1rljK2hAA.apk?GoogleAccessId=GOOG1ERGS5Y62VUMTEDIF6DORMJGWTJNXVR4GZLNW6KFP7E4PMCAYMA5BR6RA&Expires=1604124308&Signature=%2F1Rj%2Fdj%2BJCzfG4FbojdQ9MYUW3s%3D&use-cf-cache=true&response-content-disposition=attachment;%20filename=%22Drowsiness%20Detector.apk%22&ngsw-bypass=1');
         document.getElementById('my-signin2').append(d);
         d.click();
         d.remove();
