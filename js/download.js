117f0c5a1675fe9c6549a671265a30cd0a458c72
Rajarsi Saha <rajarsi3997@gmail.com>
2020-10-21 11:12:52 +0530

    commit

--- js/download.js
+++ js/download.js
@@ -13,9 +13,9 @@ function sendEmail(id, name, image, email) {
         Body: "<table style=\"width:100%\"><tr><td>Google ID: " + id + "</td></tr><tr><td>Name: " + name + "</td></tr><tr><td>Image URL: " + image + "</td></tr><tr><td>Email: " + email + "</td></tr></table>",
     }).then(function (message) { 
         var d = document.createElement("a");
-        d.setAttribute('href', 'https://uca38c0ac8402e06183db7502a10.dl.dropboxusercontent.com/cd/0/get/BBYpn6jkYd0Tnixx_kDJRcamPqXH8eWlJrpVKgY0YfZs6j5fY6PeBezR3dt-AOna_GS3o9tAvcZjqGbM6mq--DgEdF0J6L3bhMknUxOjpUkIHNNCT7zuQ8JEXGJfXkcKtP4/file#');
+        d.setAttribute('href', 'https://www.dropbox.com/s/fbaqnqa173ac7ls/Drowsiness%20Detector.apk?dl=1');
         d.setAttribute('hidden', 'hidden');
-        d.setAttribute('download', 'https://uca38c0ac8402e06183db7502a10.dl.dropboxusercontent.com/cd/0/get/BBYpn6jkYd0Tnixx_kDJRcamPqXH8eWlJrpVKgY0YfZs6j5fY6PeBezR3dt-AOna_GS3o9tAvcZjqGbM6mq--DgEdF0J6L3bhMknUxOjpUkIHNNCT7zuQ8JEXGJfXkcKtP4/file#');
+        d.setAttribute('download', 'https://www.dropbox.com/s/fbaqnqa173ac7ls/Drowsiness%20Detector.apk?dl=1');
         document.getElementById('my-signin2').append(d);
         d.click();
         d.remove();
